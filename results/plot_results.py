0093e6ce2744e10c69c40d78993fdddd945a07e7
ophid1an <ophid1an@users.noreply.github.com>
2018-05-13 17:49:24 +0300

    Use pyplot.subplots()

--- results/plot_results.py
+++ results/plot_results.py
@@ -8,30 +8,31 @@ plot_lines = int(data.shape[0] / THREADS)
 markers = ['o', 'v', '^', '<', '>', 's', 'p', '*', 'h',
            'H', '+', 'x', 'D', '8', '1', '2', '3', '4', 'd']
 arr_lengths = []
-subp_layout_right = 0.9
+subp_layout_right = 0.8
+
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 
 plt.suptitle('Speedup and efficiency')
 plt.subplots_adjust(right=subp_layout_right)
 
-plt.subplot(211)
 for line in range(plot_lines):
-    plt.plot('Threads', 'Speedup', linestyle='--', marker=markers[line], antialiased=True,
+    ax1.plot('Threads', 'Speedup', linestyle='--', marker=markers[line], antialiased=True,
              linewidth=0.7, data=data.iloc[line * THREADS + 1:(line + 1) * THREADS])
     arr_lengths.append(data.iloc[line * THREADS]['Array length'])
-plt.ylabel('Speedup')
-plt.ylim((1, THREADS))
-plt.grid(linestyle='--', linewidth=0.5, axis='y')
+ax1.set_ylabel('Speedup')
+ax1.set_ylim((1, THREADS))
+ax1.grid(linestyle='--', linewidth=0.5, axis='y')
 
-plt.subplot(212)
 for line in range(plot_lines):
-    plt.plot('Threads', 'Efficiency', linestyle='--', marker=markers[line], antialiased=True,
+    ax2.plot('Threads', 'Efficiency', linestyle='--', marker=markers[line], antialiased=True,
              linewidth=0.7, data=data.iloc[line * THREADS + 1:(line + 1) * THREADS])
-plt.ylabel('Efficiency')
-plt.xlabel('Number of threads')
-plt.grid(linestyle='--', linewidth=0.5, axis='y')
+ax2.set_ylabel('Efficiency')
+ax2.set_xlabel('Number of threads')
+ax2.grid(linestyle='--', linewidth=0.5, axis='y')
 
-plt.legend(arr_lengths, title='Array length', bbox_to_anchor=(subp_layout_right + 0.005, 0.5), loc='center left',
+fig.legend(arr_lengths, title='Array length', bbox_to_anchor=(subp_layout_right + 0.005, 0.5), loc='center left',
            ncol=2, bbox_transform=plt.gcf().transFigure)
 
-plt.savefig(filename.split(sep='.')[0] + '.png', bbox_inches="tight")
-plt.show()
+fig.savefig(filename.split(sep='.')[0] + '.png', bbox_inches="tight", dpi='figure', transparent=True)
+
+# plt.show()
