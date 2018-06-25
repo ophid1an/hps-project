30aad35a4a74d0f5c05758809a3ccff8afc6ccc7
ophid1an <ophid1an@users.noreply.github.com>
2018-06-25 11:42:46 +0300

    Implement command line options parsing using getopt()
    Also don't include seed in results csv since it is fixed and
    if number of threads is specified, don't calculate and output
    speedup and efficiency

--- src/main_omp.c
+++ src/main_omp.c
@@ -1,4 +1,5 @@
 #include <float.h>
+#include <getopt.h>
 #include <omp.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -18,45 +19,45 @@ struct result {
 
 static void calc(struct result *res, uint32_t *arr, size_t n, uint8_t b,
     uint32_t cnt, uint8_t n_threads);
-static void print_results(const struct result *res, uint8_t p, uint8_t b, unsigned seed,
-    uint8_t run, uint8_t n_threads, FILE *fptr, uint8_t first_call);
+static void print_results(const struct result *res, uint8_t p, uint8_t b,
+    uint8_t run, uint8_t n_threads, FILE *fptr, uint8_t specified_n_threads, uint8_t first_call);
 
 int main(int argc, char *argv[])
 {
     static const unsigned seed = 1;
-    static const char *filename = "results.csv";
+    static const char *filename = "results_omp.csv";
 
     static uint8_t p = 27; // Cardinality of elements 2^p , p -> [4..30]
     static uint8_t b = 14; // Cardinality of registers 2^b , b -> [4..16]
     static uint8_t runs = 1;
     static uint8_t n_threads = 0;
 
-    if (argc >= 2) {
-        p = strtoul(argv[1], NULL, 10);
-    }
-
-    if (argc >= 3) {
-        b = strtoul(argv[2], NULL, 10);
-    }
-
-    if (argc >= 4) {
-        runs = strtoul(argv[3], NULL, 10);
-    }
-
-    if (argc >= 5) {
-        filename = argv[4];
-    }
-
-    if (argc >= 6) {
-        n_threads = strtoul(argv[5], NULL, 10);
-    }
+    // Parse command line options
+    static int c;
+    while ((c = getopt(argc, argv, "p:b:r:t:")) != -1)
+        switch (c) {
+        case 'p':
+            p = strtoul(optarg, NULL, 10);
+            break;
+        case 'b':
+            b = strtoul(optarg, NULL, 10);
+            break;
+        case 'r':
+            runs = strtoul(optarg, NULL, 10);
+            break;
+        case 't':
+            n_threads = strtoul(optarg, NULL, 10);
+            break;
+        default:
+            return EXIT_FAILURE;
+        }
 
     uint8_t max_threads = omp_get_num_procs();
 
     FILE *fptr = fopen(filename, "w");
     if (fptr == NULL) {
         printf("Error opening file!\n");
-        return (EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
 
     const size_t n = 1UL << p;
@@ -96,7 +97,9 @@ int main(int argc, char *argv[])
     uint8_t first_call_to_print = 1;
     for (uint8_t r = 1; r <= runs; ++r) {
         uint8_t current_thread = 1;
+        uint8_t specified_n_threads = 0;
         if (n_threads != 0) {
+            specified_n_threads = 1;
             current_thread = n_threads;
             max_threads = n_threads;
         }
@@ -104,7 +107,7 @@ int main(int argc, char *argv[])
             printf("\nRun %u, using %u thread(s).\n", r, current_thread);
 
             calc(&res, arr, n, b, cnt, current_thread);
-            print_results(&res, p, b, seed, r, current_thread, fptr, first_call_to_print);
+            print_results(&res, p, b, r, current_thread, fptr, specified_n_threads, first_call_to_print);
             first_call_to_print = 0;
         }
     }
@@ -130,18 +133,23 @@ static void calc(struct result *res, uint32_t *arr, size_t n, uint8_t b,
     res->perc_error = ABS((cnt - res->estimate) * 100 / cnt);
 }
 
-static void print_results(const struct result *res, uint8_t p, uint8_t b, unsigned seed,
-    uint8_t run, uint8_t n_threads, FILE *fptr, uint8_t first_call)
+static void print_results(const struct result *res, uint8_t p, uint8_t b,
+    uint8_t run, uint8_t n_threads, FILE *fptr, uint8_t specified_n_threads, uint8_t first_call)
 {
-    double speedup = res->time_spent_one_thread / res->time_spent;
-    double efficiency = speedup / n_threads;
+
     printf("Estimate: %f\n", res->estimate);
     printf("Percent error: %.3f\n", res->perc_error);
     printf("Time in seconds: %.3f\n", res->time_spent);
-    printf("Speedup: %.3f\n", speedup);
-    printf("Efficiency: %.3f\n", efficiency);
-    if (first_call == 1) {
-        fprintf(fptr, "Array length,Registers,Seed,Run,Threads,Time,Percent error\n");
+    // If number of threads is specified, don't calculate and print
+    // speedup and efficiency since there is no baseline
+    if (specified_n_threads != 1) {
+        double speedup = res->time_spent_one_thread / res->time_spent;
+        double efficiency = speedup / n_threads;
+        printf("Speedup: %.3f\n", speedup);
+        printf("Efficiency: %.3f\n", efficiency);
     }
-    fprintf(fptr, "%u,%u,%u,%u,%u,%.*g,%f\n", p, b, seed, run, n_threads, DBL_DIG, res->time_spent, res->perc_error);
+    if (first_call == 1) {
+        fprintf(fptr, "Array length,Registers,Run,Threads,Time,Percent error\n");
+    }
+    fprintf(fptr, "%u,%u,%u,%u,%.*g,%f\n", p, b, run, n_threads, DBL_DIG, res->time_spent, res->perc_error);
 }
