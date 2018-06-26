149dceefcc5f03968384606f6e6429f1480f6cc9
ophid1an <ophid1an@users.noreply.github.com>
2018-06-26 08:55:12 +0300

    Fix print_results(), adjust buffer size to be a maximum of option u

--- src/main_omp.c
+++ src/main_omp.c
@@ -10,6 +10,7 @@
 #include "hllpp_omp.h"
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
 
 struct result {
     double estimate;
@@ -19,7 +20,7 @@ struct result {
 };
 
 static void print_results(const struct result *res, uint8_t p, uint8_t b,
-    uint8_t run, uint8_t n_threads, FILE *fptr, uint8_t first_call);
+    uint8_t run, uint8_t n_threads, uint8_t current_n_threads, FILE *fptr, uint8_t first_call);
 
 int main(int argc, char *argv[])
 {
@@ -27,8 +28,10 @@ int main(int argc, char *argv[])
 
     static uint8_t p = 27; // Cardinality of elements 2^p , p -> [4..30]
     static uint8_t b = 14; // Cardinality of registers 2^b , b -> [4..16]
-    static uint32_t u = 128; // Size of buffer in MiBs
+    static uint32_t u = 256; // Size of buffer in MiBs
     static uint8_t runs = 1;
+    // If n_threads is set to 0 apply algorithms using
+    // 1 up to omp_get_num_procs()
     static uint8_t n_threads = 0;
 
     // Parse command line options
@@ -77,11 +80,15 @@ int main(int argc, char *argv[])
 
     const uint32_t mask = 1UL * n + (n - 1UL);
 
-    // Print distinct elements number of array
+    // Print the number of the distinct elements of array
     printf("Number of distinct elements: %zu\n", cnt);
     printf("Percentage: %.3f\n", cnt * 100.0 / n);
 
+    // Set buffer length equal to the minimum of
+    // the specified size and the integer array size
     size_t buf_length = (1UL << 20) * u / sizeof(uint32_t);
+    buf_length = MIN(buf_length, n);
+
     uint32_t *buf = malloc(sizeof *buf * buf_length);
     if (buf == NULL) {
         fprintf(stderr, "Fatal: failed to allocate memory.\n");
@@ -138,7 +145,7 @@ int main(int argc, char *argv[])
             }
             res.perc_error = ABS((cnt - res.estimate) * 100 / cnt);
 
-            print_results(&res, p, b, r, current_n_threads, fptr, first_call_to_print);
+            print_results(&res, p, b, r, n_threads, current_n_threads, fptr, first_call_to_print);
             first_call_to_print = 0;
         }
     }
@@ -150,7 +157,7 @@ int main(int argc, char *argv[])
 }
 
 static void print_results(const struct result *res, uint8_t p, uint8_t b,
-    uint8_t run, uint8_t n_threads, FILE *fptr, uint8_t first_call)
+    uint8_t run, uint8_t n_threads, uint8_t current_n_threads, FILE *fptr, uint8_t first_call)
 {
 
     printf("Estimate: %f\n", res->estimate);
@@ -160,12 +167,12 @@ static void print_results(const struct result *res, uint8_t p, uint8_t b,
     // speedup and efficiency since there is no baseline
     if (n_threads == 0) {
         double speedup = res->time_spent_one_thread / res->time_spent;
-        double efficiency = speedup / n_threads;
+        double efficiency = speedup / current_n_threads;
         printf("Speedup: %.3f\n", speedup);
         printf("Efficiency: %.3f\n", efficiency);
     }
     if (first_call == 1) {
         fprintf(fptr, "Array length,Registers,Run,Threads,Time,Percent error\n");
     }
-    fprintf(fptr, "%u,%u,%u,%u,%.*g,%f\n", p, b, run, n_threads, DBL_DIG, res->time_spent, res->perc_error);
+    fprintf(fptr, "%u,%u,%u,%u,%.*g,%f\n", p, b, run, current_n_threads, DBL_DIG, res->time_spent, res->perc_error);
 }
