9d348537e444c5eec5cf93d70cd3874b815fd3c2
ophid1an <ophid1an@users.noreply.github.com>
2018-06-30 20:11:07 +0300

    Update convert and plot results scripts to work with Open MPI too

--- results/convert_results.py
+++ results/convert_results.py
@@ -14,9 +14,10 @@ data = pd.read_csv(filename)
 
 arr_lengths = np.sort(data['Array length'].unique())
 runs = data['Run'].max()
-threads = data['Threads'].max()
+threads_or_tasks_str = 'Threads' if data.columns.contains('Threads') else 'Tasks'
+threads_or_tasks = data[threads_or_tasks_str].max()
 
-if len(arr_lengths) * runs * threads != data.shape[0]:
+if len(arr_lengths) * runs * threads_or_tasks != data.shape[0]:
     print("Invalid csv file.")
     sys.exit(1)
 
@@ -24,28 +25,29 @@ data_converted = pd.DataFrame(
     columns=list(data.drop(labels=['Registers', 'Run'], axis=1).columns) + ['Speedup', 'Efficiency'])
 
 for arr_len in arr_lengths:
-    for thread in range(1, threads + 1):
-        temp_df = data[(data['Array length'] == arr_len) & (data['Threads'] == thread)]
+    for t in range(1, threads_or_tasks + 1):
+        temp_df = data[(data['Array length'] == arr_len) & (data[threads_or_tasks_str] == t)]
         trim_mean_time = stats.trim_mean(temp_df['Time'], 0.25)  # Get interquartile mean of times
         data_converted = data_converted.append(
-            pd.DataFrame([[arr_len, thread, trim_mean_time, temp_df['Percent error'].iloc[0], 1.0, 1.0]],
+            pd.DataFrame([[arr_len, t, trim_mean_time, temp_df['Percent error'].iloc[0], 1.0, 1.0]],
                          columns=data_converted.columns), ignore_index=True)
 
-if threads > 1:
+if threads_or_tasks > 1:
     for arr_len in arr_lengths:
-        time_one_thread = \
-            data_converted[(data_converted['Array length'] == arr_len) & (data_converted['Threads'] == 1)]['Time'].iloc[
+        time_one_t = \
+            data_converted[(data_converted['Array length'] == arr_len) & (data_converted[threads_or_tasks_str] == 1)][
+                'Time'].iloc[
                 0]
-        for thread in range(2, threads + 1):
+        for t in range(2, threads_or_tasks + 1):
             idx = data_converted.index[
-                (data_converted['Array length'] == arr_len) & (data_converted['Threads'] == thread)]
-            data_converted.loc[idx, 'Speedup'] = time_one_thread / data_converted.loc[idx, 'Time']
-            data_converted.loc[idx, 'Efficiency'] = data_converted.loc[idx, 'Speedup'] / thread
+                (data_converted['Array length'] == arr_len) & (data_converted[threads_or_tasks_str] == t)]
+            data_converted.loc[idx, 'Speedup'] = time_one_t / data_converted.loc[idx, 'Time']
+            data_converted.loc[idx, 'Efficiency'] = data_converted.loc[idx, 'Speedup'] / t
 
 data_converted['Array length'] = data_converted['Array length'].astype(str)
 for idx in data_converted.index.values:
     data_converted.loc[idx, 'Array length'] = '2^' + data_converted.loc[idx, 'Array length']
 
 data_converted.to_csv(filename.split('.')[0] + '_converted.csv',
-                      columns=['Array length', 'Threads', 'Time', 'Speedup', 'Efficiency', 'Percent error'],
+                      columns=['Array length', threads_or_tasks_str, 'Time', 'Speedup', 'Efficiency', 'Percent error'],
                       index=False, float_format='%.3f')
--- results/plot_results.py
+++ results/plot_results.py
@@ -10,8 +10,9 @@ if len(sys.argv) != 2 or not re.search('\.csv$', sys.argv[-1]):
 
 filename = sys.argv[-1]
 data = pd.read_csv(filename)
-threads = data['Threads'].max()
-plot_lines = int(data.shape[0] / threads)
+threads_or_tasks_str = 'Threads' if data.columns.contains('Threads') else 'Tasks'
+threads_or_tasks = data[threads_or_tasks_str].max()
+plot_lines = int(data.shape[0] / threads_or_tasks)
 markers = ['o', 'v', '^', '<', '>', 's', 'p', '*', 'h',
            'H', '+', 'x', 'D', '8', '1', '2', '3', '4', 'd']
 arr_lengths = []
@@ -23,18 +24,18 @@ plt.suptitle('Speedup and efficiency')
 plt.subplots_adjust(right=subp_layout_right)
 
 for line in range(plot_lines):
-    ax1.plot('Threads', 'Speedup', linestyle='--', marker=markers[line], antialiased=True,
-             linewidth=0.7, data=data.iloc[line * threads + 1:(line + 1) * threads])
-    arr_lengths.append(data.iloc[line * threads]['Array length'])
+    ax1.plot(threads_or_tasks_str, 'Speedup', linestyle='--', marker=markers[line], antialiased=True,
+             linewidth=0.7, data=data.iloc[line * threads_or_tasks + 1:(line + 1) * threads_or_tasks])
+    arr_lengths.append(data.iloc[line * threads_or_tasks]['Array length'])
 ax1.set_ylabel('Speedup')
-ax1.set_ylim((1, threads))
+ax1.set_ylim((1, threads_or_tasks))
 ax1.grid(linestyle='--', linewidth=0.5, axis='y')
 
 for line in range(plot_lines):
-    ax2.plot('Threads', 'Efficiency', linestyle='--', marker=markers[line], antialiased=True,
-             linewidth=0.7, data=data.iloc[line * threads + 1:(line + 1) * threads])
+    ax2.plot(threads_or_tasks_str, 'Efficiency', linestyle='--', marker=markers[line], antialiased=True,
+             linewidth=0.7, data=data.iloc[line * threads_or_tasks + 1:(line + 1) * threads_or_tasks])
 ax2.set_ylabel('Efficiency')
-ax2.set_xlabel('Number of threads')
+ax2.set_xlabel('Number of ' + threads_or_tasks_str.lower())
 ax2.grid(linestyle='--', linewidth=0.5, axis='y')
 
 fig.legend(arr_lengths, title='Array length', bbox_to_anchor=(subp_layout_right + 0.005, 0.5), loc='center left',
